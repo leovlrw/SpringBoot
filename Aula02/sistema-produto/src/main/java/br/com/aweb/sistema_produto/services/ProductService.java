9fc1424640c0f29155b7e3335f3630382b228a14
leovlrw <leonardovalerio89@gmail.com>
2025-08-22 22:32:24 -0300

    aula02

---
+++ Aula02/sistema-produto/src/main/java/br/com/aweb/sistema_produto/services/ProductService.java
@@ -0,0 +1,42 @@
+package br.com.aweb.sistema_produto.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.aweb.sistema_produto.model.Product;
+import br.com.aweb.sistema_produto.repository.ProductRepository;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    // listar todos os produtos
+    public List<Product> listAll() {
+        return productRepository.findAll();
+    }
+
+    public Product findProduct(Long id) {
+        Optional<Product> product = productRepository.findById(id);
+        if (product.isPresent()) {
+            return product.get();
+        }
+        throw new RuntimeException("Produto não encontrado!");
+    }
+
+    // cadastrar/alterar produto
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    // remover produto
+    public void deleteProduct(Long id){
+        if (!productRepository.existsById(id))
+            throw new RuntimeException("Erro ao excluir produto!");
+        productRepository.deleteById(id);
+    }
+}
---
+++ Aula02/sistema-produto/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO product (name, price, description) VALUES
+    ('Notebook', 3500.00, 'Notebook 16GBRAM, SSD 512GB'),
+    ('Smartphone', 1999.90, 'Tela 6.5", 128GB'),
+    ('Tablet', 1200.50 , 'Tela 10", 64GB'),
+    ('Monitor', 899.00, 'Monitor 24" Full HD'),
+    ('Teclado', 199.90, 'Teclado mecânico RGB');
