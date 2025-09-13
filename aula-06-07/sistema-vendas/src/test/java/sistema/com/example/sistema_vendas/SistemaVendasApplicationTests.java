990d688d554045ebb3d736147c9e88c78b1e0c64
Leonardo Valério <leonardovalerio89@gmail.com>
2025-09-12 22:53:00 -0300

    sistemavenda

---
+++ aula-06-07/sistema-vendas/src/test/java/sistema/com/example/sistema_vendas/SistemaVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package sistema.com.example.sistema_vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
